38975df0f9c87ac6c62afa5be3455ac993457217
Ishaan Bhimwal <ishaanbhimwal@protonmail.com>
2022-09-23 16:52:19 +0530

    remove extra slide in presentprojects

--- src/Project/PresentProjectPage.js
+++ src/Project/PresentProjectPage.js
@@ -25,31 +25,23 @@ import HAL from "../helpers/images/HAL.PNG";
 const pics = [
   { img: doggo, id: 1 },
   { img: HAL, id: 2 },
-  { img: doggo, id: 3 },
 ];
 const headers = [
-  { title: "Doggo", id: 4 },
-  { title: "High-speed Autonomous Logistics", id: 5 },
-  { title: "Doggo", id: 6 },
+  { title: "Doggo", id: 3 },
+  { title: "High-speed Autonomous Logistics", id: 4 },
 ];
 const paras = [
   {
     title: "Doggo",
     content:
       " We participate every year in The University Rover Challange(URC) which is an annual international competetion hosted by the Mars Society for university and college teams to fabricate a prototype Mars Rover, which will then be subjected to a series of tasks and challenges.The competition pushes the teams to the extreme limits of engineering, management and proactively work towards the motto of the challenge 'Think, Design and Engineer'",
-    id: 7,
+    id: 5,
   },
   {
     title: "HAL",
     content:
       " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    id: 8,
-  },
-  {
-    title: "HAL",
-    content:
-      " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ",
-    id: 9,
+    id: 6,
   },
 ];
 
@@ -89,9 +81,11 @@ function PresentProjectPage() {
               enabled: false,
             }}
             direction={"vertical"}
-            modules={[Controller,]}
+            modules={[Controller]}
             onSwiper={setFirstSwiper}
-            controller={{control: [thirdSwiper, controlledSwiper, thirdSwiper]}}
+            controller={{
+              control: [thirdSwiper, controlledSwiper, thirdSwiper],
+            }}
             className="projectslide_fibonacci_holder"
           >
             {pics.map((pic) => (
@@ -129,18 +123,19 @@ function PresentProjectPage() {
             pagination={{
               clickable: true,
             }}
-            
             autoplay={true}
             modules={[Controller, Keyboard, Pagination, Autoplay]}
             onSwiper={setThirdSwiper}
-            controller={{control: [firstSwiper, controlledSwiper, firstSwiper] }}
+            controller={{
+              control: [firstSwiper, controlledSwiper, firstSwiper],
+            }}
             className="projectslide_description"
           >
             {paras.map((para) => (
               <SwiperSlide key={para.id}>
                 <div>
-                <h1>{para.title}</h1>
-                <p>{para.content}</p>
+                  <h1>{para.title}</h1>
+                  <p>{para.content}</p>
                 </div>
               </SwiperSlide>
             ))}
@@ -148,12 +143,11 @@ function PresentProjectPage() {
         </div>
       </div>
       <div>
-          <div className="spacer"></div>
-          <Footer />
+        <div className="spacer"></div>
+        <Footer />
       </div>
     </>
   );
-};
-
+}
 
 export default PresentProjectPage;
