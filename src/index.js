f800b63fef9bfc939b899bb6a922e624321cf18c
Ishaan Bhimwal <ishaanbhimwal@protonmail.com>
2022-09-25 00:52:17 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./helpers/css/background.css";
-import "fontsource-roboto-slab"
+import "fontsource-play"
 
 ReactDOM.render(
   <React.StrictMode>
