0519790522718b46ba13124034b2947673e59b4a
Ishaan Bhimwal <ishaanbhimwal@protonmail.com>
2022-09-25 15:20:22 +0530

    minor changes 2

--- src/Project/Model.js
+++ src/Project/Model.js
@@ -2,14 +2,10 @@ import React, { useRef, useState } from "react";
 import { useGLTF } from "@react-three/drei";
 import doggo from "../helpers/models/doggo.gltf";
 import { useFrame } from "@react-three/fiber";
-import { Color, MeshBasicMaterial } from "three";
 
 export default function Model(props) {
   const { nodes, materials } = useGLTF(doggo);
   const boxRef = useRef();
-
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
 
   useFrame(() => {
     boxRef.current.rotation.y += 0.001;
@@ -22,7 +18,7 @@ export default function Model(props) {
         receiveShadow
         geometry={nodes.doggo.geometry}
         ref={boxRef}
-        rotation-x={Math.PI * 0.25}
+        rotation-x={Math.PI * 0.1}
       >
         <meshStandardMaterial color={"#2f3241"} />
       </mesh>
