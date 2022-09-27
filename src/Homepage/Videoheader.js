254e792b206fdafa8c0cfe7417c8abba33bbde11
Ishaan Bhimwal <ishaanbhimwal@protonmail.com>
2022-09-27 21:26:53 +0530

    minor changes 6

--- src/Homepage/Videoheader.js
+++ src/Homepage/Videoheader.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import video from "../helpers/images/video.webm";
 import "../helpers/css/videoheader.css";
 function Videoheader() {
   return (
     <div className="videohead">
-      <video autoPlay  muted class="video-parent">
+      <video autoPlay muted class="video-parent">
         <source src={video} type="video/webm" />
       </video>
-      <div className="video_text">
-      </div>
     </div>
   );
 }
