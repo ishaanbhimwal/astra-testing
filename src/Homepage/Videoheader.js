f5f60dd3a7d3b7a5e768334ac49c908d4781fed6
Jackhammer <49639514+JackhammerYT@users.noreply.github.com>
2022-09-28 00:05:32 +0530

    scroll icon

--- src/Homepage/Videoheader.js
+++ src/Homepage/Videoheader.js
@@ -2,14 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import video from "../helpers/images/video.webm";
 import "../helpers/css/videoheader.css";
+import scroll from  "../helpers/images/scroll.png"
+import {useEffect} from 'react';
+import $ from 'jquery';
+
+const delay = ms => new Promise(
+  resolve => setTimeout(resolve, ms)
+);
+
+
 function Videoheader() {
+  useEffect(() => {
+    async function makeRequest() {
+      await delay(10000);
+      let scroll = document.getElementsByClassName("scroll_image")[0]
+      $(scroll).fadeIn(2000)
+    }
+
+    makeRequest();
+  });
   return (
     <div className="videohead">
       <video autoPlay  muted class="video-parent">
         <source src={video} type="video/webm" />
       </video>
-      <div className="video_text">
+      <div className="scroll_image">
+        <img src={scroll}></img>
       </div>
+
     </div>
   );
 }
